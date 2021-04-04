c6c796495f3bb31ce074e27571bdef10a99f16dc
Manu <nox@pop-os>
2021-04-04 15:17:45 +0200

    Notification working

---
+++ DiscordBot.py
@@ -0,0 +1,29 @@
+from discord import Client
+
+
+class DiscordBot(Client):
+
+    def __init__(self, queue, channel_id):
+        super(DiscordBot, self).__init__()
+        self.queue = queue
+        self.channel_id = channel_id
+
+    async def on_ready(self):
+        print(f'{self.user} is ready, waiting...')
+        match = self.queue.get()
+        await self.get_channel(self.channel_id).send(match)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+
+
+async def start(queue, token, channel_id):
+    await DiscordBot(queue, channel_id).start(token)
+
+
+def run_it_forever(loop):
+    loop.run_forever()
--- league-auto-accept.py
+++ league-auto-accept.py
@@ -1,22 +1,48 @@
 import pyautogui
 import time
 from datetime import datetime
+from threading import Thread
+from queue import Queue
+import asyncio
+import os
+import DiscordBot
 
-while True:
 
-    accept = pyautogui.locateCenterOnScreen('accept.png', confidence=0.7)
-    decline = pyautogui.locateCenterOnScreen('decline.png', confidence=0.7)
+class Accept(Thread):
 
-    if accept:
-        pyautogui.moveTo(accept)
-        # click multiple times just to make sure accept button gets clicked
-        pyautogui.click(accept)
+    def __init__(self, queue):
+        super(Accept, self).__init__()
+        self.queue = queue
 
-        print(f'Game Accepted. {datetime.now()}')
+    def run(self):
+        while True:
 
-        # Discord stuff
+            accept = pyautogui.locateCenterOnScreen('accept.png', confidence=0.7)
+            # decline = pyautogui.locateCenterOnScreen('decline.png', confidence=0.7)
 
-        time.sleep(5)
-    else:
-        print(datetime.now())
-        time.sleep(1)
+            if accept:
+                pyautogui.moveTo(accept)
+                # click multiple times just to make sure accept button gets clicked
+                pyautogui.click(accept)
+
+                print(f'Game Accepted. {datetime.now()}')
+
+                self.queue.put("MATCH")
+
+                time.sleep(5)
+            else:
+                print(datetime.now())
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+
+    queue = Queue()
+
+    asyncio.get_child_watcher()
+    loop = asyncio.get_event_loop()
+    loop.create_task(DiscordBot.start(queue, os.getenv('TOKEN'), 365243942771490829))
+    bot = Thread(target=DiscordBot.run_it_forever, args=(loop,))
+
+    bot.start()
+    auto_accept = Accept(queue).start()
